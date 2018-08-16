7bd617c144d54f711e18d0b712181e57adb038e1
Rafael Rollo <rafaelrollo92@gmail.com>
2018-08-16 02:37:32 -0300

    Init

---
+++ src/main/java/br/com/trollo/project/TrolloApplication.java
@@ -0,0 +1,12 @@
+package br.com.trollo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrolloApplication.class, args);
+	}
+}
